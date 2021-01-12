77ab88d400089df76ee863fd5836f339012c7eeb
Tarzuto <tarzuto@gmail.com>
2021-01-12 19:52:58 +0700

    Print JAVA

---
+++ 0003-JAVA/src/com/zantaclaus/Main.java
@@ -0,0 +1,11 @@
+package com.zantaclaus;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("   J   a  v     v  a");
+        System.out.println("   j  a a  v   v  a a");
+        System.out.println("j  j aaaaa  v v  aaaaa");
+        System.out.println(" jj a     a  v  a     a");
+    }
+}
